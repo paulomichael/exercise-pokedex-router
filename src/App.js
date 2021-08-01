e3f2278edd335578a39ddcc617f3cc9110147afa
paulomichael <paulo.michael@gmail.com>
2021-08-01 01:50:58 -0300

    exercicio 1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,54 @@
-import React from 'react';
-import './App.css';
-import pokemons from './data';
-import Pokedex from './Pokedex';
+import React, { Component } from "react";
+import "./App.css";
+import pokemons from "./data";
+import Pokedex from "./Pokedex";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
- // comment for the initial commit
-function App() {
-  return (
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={(routeProps) => (
+              <div className="App">
+                <h1> Pokedex </h1>
+                <Pokedex {...routeProps} pokemons={pokemons} />
+              </div>
+            )}
+          />
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+export default App;
+/*   return (
     <div className="App">
       <h1> Pokedex </h1>
       <Pokedex pokemons={pokemons} />
     </div>
   );
-}
 
-export default App;
+*/
